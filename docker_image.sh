d5235816e149653c7e7efc2689a733d5b4f3b029
IgorKruglov921 <iakruglov@mail.ru>
2020-11-06 12:58:58 +0300

    Поиск по файлу исключения и дате
    Co-Authored-By: IgorKruglov921 <34272117+IgorKruglov921@users.noreply.github.com>

--- docker_image.sh
+++ docker_image.sh
@@ -1,2 +1,28 @@
 # !/bin/bash
-echo "Hello World"
+no_delete=no_delete.txt
+date
+DATA=$(date +"%Y-%m-%d" -d "-150 days");
+IFS=$'\n'
+echo $DATA;
+echo
+for image in $(docker images --format "table{{.Repository}}\t{{.Tag}}\t{{.ID}}\t{{.CreatedAt}}" | awk '{print $3}'); do
+	if grep $image $no_delete;
+	then
+		echo "NO_DELETE_IMAGE" $image
+
+	else
+		DATA1=$(docker inspect -f "{{.Created}}" $image)
+		DATA2="${DATA1:0:10}"
+		echo
+		echo $DATA2;
+		if [[ $DATA2 > $DATA ]]; then
+			echo "NO_DELETE_IMAGE" $image
+			echo
+		else
+			echo "DELETE_IMAGE" $image
+#			(docker rmi $image --force)
+			echo
+		fi
+
+	fi
+done
